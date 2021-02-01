69b0d46104b760eae9f903af5bf73b3ae4e8e7aa
DictatorOfCyber <78338432+DictatorOfCyber@users.noreply.github.com>
2021-02-01 12:15:15 +0600

    Create math.py

---
+++ math.py
@@ -0,0 +1,21 @@
+if choice == 1:
+
+	print(" ")	from Files import odd
+
+	from file import odd
+
+	print(odd)
+
+elif choice == 2:
+
+	print(" ")
+
+	from Files import even
+
+	from file import even
+
+	print(even)
+
+else:
+
+	print (" ")
