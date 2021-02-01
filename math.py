9f3dfd653e9985f208c3b98d3b7c9738869b35cf
DictatorOfCyber <78338432+DictatorOfCyber@users.noreply.github.com>
2021-02-01 12:27:48 +0600

    Update math.py

--- math.py
+++ math.py
@@ -1,23 +1,17 @@
+print("Odd Number")
+print(" ")
+print("Even Number")
+print(" ")
+choice = int(input(" Please select the category 1 or 2 : "))
+
 if choice == 1:
-
-	print(" ")	
-        
-        from files import odd
-
-	from file import odd
-
-	print(odd)
-
-elif choice == 2:
-
 	print(" ")
-
+	from file import odd
+	print(odd)
+elif choice ==2:
+	print(" ")
 	from file import even
-
-	from file import even
-
 	print(even)
-
 else:
-
 	print (" ")
+	print("Invalid number Input")
